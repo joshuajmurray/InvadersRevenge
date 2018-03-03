a42f3935530ba2c62cfbb75d3117e87e0f85606d
Dylan Drudge <drudged@oregonstate.edu>
2018-03-03 12:18:01 -0800

    add player class
    add a player class that can move and shoot.

---
+++ src/edu/osucascades/cs361/chi/PlayerTank.java
@@ -0,0 +1,58 @@
+package edu.osucascades.cs361.chi;
+
+import processing.core.PApplet;
+
+public class PlayerTank{
+    public int x = 0;
+    public int y = 0;
+    private PApplet canvas;
+    private int xSpeed = 0;
+    private boolean reloading;
+
+    public void setReloading(boolean b) {
+        this.reloading = b;
+    }
+
+    public boolean isReloading() {
+        return reloading;
+    }
+
+    public PlayerTank(int x, int y, PApplet canvas){
+    this.x = x;
+    this.y = y;
+    this.canvas = canvas;
+    }
+    public void move(int direction){
+        this.xSpeed = direction * 5;
+    }
+
+
+    private void update(){
+        if (this.x < 0){
+            this.x = this.canvas.width;
+        }
+
+        if (this.x > this.canvas.width)
+        {
+            this.x = 0;
+        }
+        this.x = this.x + this.xSpeed;
+    }
+
+    public void display (){
+        this.update();
+        canvas.rect(this.x,this.y,50,15);
+        canvas.fill(255,0,0);
+    }
+
+     public void shoot(Sketch canvas){
+        int rocketX = this.x + 24;
+        int rocketY = this.y - 10;
+        playerRocket rocket = new playerRocket(rocketX,rocketY, canvas, this);
+        rocket.launch(this);
+
+     }
+
+
+
+}
