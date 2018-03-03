477dbbba50444e5311efa082867e8274ae4b3bb8
Dylan Drudge <drudged@oregonstate.edu>
2018-03-03 12:21:52 -0800

    Add player demo and control functions
    set up player and explosives array.
    set up player inputs A moves left D moves right SpaceBar fires a rocket.

--- src/edu/osucascades/cs361/chi/Sketch.java
+++ src/edu/osucascades/cs361/chi/Sketch.java
@@ -1,19 +1,63 @@
 package edu.osucascades.cs361.chi;
-
 import processing.core.PApplet;
+
+import java.util.ArrayList;
 
 public class Sketch extends PApplet {
 
+    private PlayerTank player;
+    private int playerMovementDirection = 0;
+
+    public ArrayList<Explosive> explosives = new ArrayList<>();
+
+    public ArrayList<Explosive> getExplosives() {
+        return this.explosives;
+    }
     public void settings() {
-        fullScreen();
+        this.size(800,800);
     }
 
     public void setup() {
-        background(55);
+        smooth();
+       this.player = new PlayerTank(0,600, this);
+
     }
 
     public void draw() {
-        fill(255, 255, 0);
-        ellipse(width/2, height/2, 100, 100);
+        background(55);
+        this.player.move(playerMovementDirection);
+        this.player.display();
+
+        //cycle through explosives
+        for (int i=0; i<this.explosives.size(); i++) {
+            Explosive projectile = this.explosives.get(i);
+            projectile.move();
+            projectile.display();
+        }
+
+    }
+    public void keyPressed()
+    {
+        if (key == 'a')
+        {
+            this.playerMovementDirection = -1;
+        }
+
+        if (key == 'd')
+        {
+            this.playerMovementDirection = 1;
+        }
+    }
+
+    public void keyReleased()
+    {
+        if (key == 'a' || key == 'd' ){
+            this.playerMovementDirection = 0;
+        }
+
+         if (key == ' ' && this.player.isReloading() == false) {
+           this.player.shoot(this);
+           }
+
     }
 }
