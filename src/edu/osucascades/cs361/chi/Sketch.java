1190dd7483a30ed8b46dba8d9848a48a9845ac82
joshua murray <joshuajmurray@gmail.com>
2018-03-05 15:54:30 -0800

    Add screen class with score and lives Fixes #13, #27, #30

--- src/edu/osucascades/cs361/chi/Sketch.java
+++ src/edu/osucascades/cs361/chi/Sketch.java
@@ -3,36 +3,19 @@ package edu.osucascades.cs361.chi;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-    public static final String JOSH_TEST = "test";
+
+    Screen s;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-        background(55);
+        s = new Screen(100000,4,this);
     }
 
     public void draw() {
-        drawRyderCircle();
-        drawCamCircle();
-
-        noFill();
-        ellipse(width - 100, height - 100, 100, 100);
-        textSize(24);
-        text(JOSH_TEST, (width - 100) - (textWidth(JOSH_TEST) / 2), height - 100, 100);
-        fill(255, 0, 255);
-        ellipse(width - 500, height - 500, 100, 100);
-
+        s.updateScreen();
     }
 
-    private void drawRyderCircle() {
-        fill(100, 0, 255);
-        ellipse(width/2 - 200, height/2, 100,100);
-    }
-
-    private void drawCamCircle() {
-        fill(0, 128, 0);
-        ellipse(width/2 - 300, height/2, 100,100);
-    }
 }
