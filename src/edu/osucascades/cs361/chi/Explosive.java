381a148b8e93bff53f7f915d8c9c68a0533bd1aa
Dylan Drudge <drudged@oregonstate.edu>
2018-03-06 13:50:22 -0800

    Add collision and getters and setters
    Fixes #32

--- src/edu/osucascades/cs361/chi/Explosive.java
+++ src/edu/osucascades/cs361/chi/Explosive.java
@@ -3,6 +3,8 @@ package edu.osucascades.cs361.chi;
 public class Explosive {
     private int x;
     private int y;
+    private int width;
+    private int height;
     private int direction;
     private Sketch canvas;
     private VehicleSuper vehicle;
@@ -27,7 +29,7 @@ public class Explosive {
 
     public void display() {
         this.move();
-        canvas.rect(this.x + vehicle.width/2, this.y - vehicle.height/2, 3, 10);
+        canvas.rect(this.x + vehicle.width/2, this.y - vehicle.height/2, width, height);
         canvas.fill(255);
     }
 
@@ -41,5 +43,22 @@ public class Explosive {
 
     void setDirection(int direction) {
         this.direction = direction;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void isCollision(Explosive  projectile, VehicleSuper vehicle) {
+        
+        if (projectile.x >= vehicle.getX() && projectile.x +  projectile.width <= vehicle.getX() + vehicle. getWidth()) {
+            if (projectile.y >= vehicle.getY() && projectile.y  + projectile.height <= vehicle.getY() +  vehicle.getHeight()) {
+                vehicle.kill();
+            }
+        }
     }
 }
