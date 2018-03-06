8468d76bc0f2626b590f8cd9242c44721293cd6c
Dylan Drudge <drudged@oregonstate.edu>
2018-03-06 13:52:15 -0800

    Add if statements controlling how vehicle super  draws and moves based on what its child class is

--- src/edu/osucascades/cs361/chi/VehicleSuper.java
+++ src/edu/osucascades/cs361/chi/VehicleSuper.java
@@ -1,16 +1,17 @@
 package edu.osucascades.cs361.chi;
 
-import processing.core.PApplet;
 
 public class VehicleSuper {
     public int x;
     public int y;
     public int width;
     public int height;
+    private int direction = -1;
 
-    private PApplet canvas;
+    private Sketch canvas;
     private int xSpeed = 0;
     private boolean reloading;
+    public char child;
 
     public void setReloading(boolean b) {
         this.reloading = b;
@@ -23,26 +24,68 @@ public class VehicleSuper {
         this.xSpeed = x * 3;
     }
 
-    public VehicleSuper(int x, int y, PApplet canvas){
+    public VehicleSuper(int x, int y, Sketch canvas){
         this.x = x;
         this.y = y;
         this.canvas = canvas;
     }
 
     private void move() {
-        if (this.x < 0) {
-            this.x = 0;
+        if (this.child == 't') {
+            if (this.x < 0) {
+                this.x = 0;
+            }
+            if (this.x > this.canvas.width) {
+                this.x = this.canvas.width;
+            }
+            this.x = this.x + this.xSpeed;
         }
-        if (this.x > this.canvas.width) {
-            this.x = this.canvas.width;
+        if (this.child == 'a'){
+            if (this.direction == 1) {
+                this.x += 1;
+                if (this.x == canvas.width - this.width) {
+                    this.direction = 0;
+                }
+            }
+            if (this.direction == 0) {
+                this.x -= 1;
+                if (this.x == 0) {
+                    this.direction = 1;
+                }
+            }
         }
-        this.x = this.x + this.xSpeed;
     }
 
     public void draw() {
-        this.move();
-        canvas.rect(this.x,this.y,this.width,this.height);
-        canvas.fill(255,0,0);
+        if (this.child == 't'){
+            this.move();
+        canvas.rect(this.x, this.y, this.width, this.height);
+        canvas.fill(255, 0, 0);
+        }
+        if (this.child == 'a'){
+            canvas.fill(0, 255, 50);
+            canvas.rect(x, y, width, height);
+            move();
+        }
+    }
+
+    public void kill(){
+        this.canvas.vehicles.remove(this);
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
     }
 
 }
