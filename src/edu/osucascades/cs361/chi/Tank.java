9291ba2f3707fe87211622ce53e0fc650f68206b
Dylan Drudge <drudged@oregonstate.edu>
2018-03-07 12:57:41 -0800

    Remove unnecessary code
    trim down VehicleSuper classes

--- src/edu/osucascades/cs361/chi/Tank.java
+++ src/edu/osucascades/cs361/chi/Tank.java
@@ -1,28 +1,22 @@
 package edu.osucascades.cs361.chi;
 
 public class Tank extends VehicleSuper {
-    private int x;
-    private int y;
+
     private int width = 50;
     private int height = 15;
     private Sketch canvas;
-    private boolean reloading;
 
     Tank(int x, int y, Sketch canvas){
-        super(x, y , canvas);
-        this.x = x;
-        this.y = y;
+        super(x, y ,'t', canvas);
         super.setHeight(this.height);
         super.setWidth(this.width);
+
         this.canvas = canvas;
-        super.child = 't';
         this.canvas.vehicles.add(this);
     }
 
     public void updateDirection(int direction){
         super.setxSpeed(direction);
-        this.x = super.getX();
-        this.y = super.getY();
     }
 
     public void shoot(Sketch canvas) {
@@ -30,11 +24,4 @@ public class Tank extends VehicleSuper {
         rocket.launch(this);
     }
 
-    public void setReloading(boolean b) {
-        this.reloading = b;
-    }
-
-    public boolean isReloading() {
-        return reloading;
-    }
 }
