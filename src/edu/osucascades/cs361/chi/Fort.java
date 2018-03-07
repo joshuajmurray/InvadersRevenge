512e0ee5ddf99f54121965a4f172659d70e36e0e
Camron Fritz <camfritz@gmail.com>
2018-03-07 15:53:17 -0800

    Add Fort kill/collidable implementation stub

--- src/edu/osucascades/cs361/chi/Fort.java
+++ src/edu/osucascades/cs361/chi/Fort.java
@@ -2,12 +2,13 @@ package edu.osucascades.cs361.chi;
 
 import processing.core.PApplet;
 
-public class Fort {
+public class Fort implements Collidable {
 
     private int x;
     private int y;
     private int lifeRemaining;
     private PApplet p;
+    public Sketch canvas;
 
     public Fort(int x, int y, int lifeRemaining, PApplet p) {
         this.x = x;
@@ -20,8 +21,17 @@ public class Fort {
         lifeRemaining -= amount;
     }
 
+    public void kill() {
+        this.canvas.forts.remove(this);
+    }
+
     public void draw() {
         p.fill(255, 0 , 0);
-        p.rect(x, y, 4, 3);
+        p.rect(x, y, 40, 30);
+    }
+
+    @Override
+    public void checkCollisions(Collidable c) {
+
     }
 }
