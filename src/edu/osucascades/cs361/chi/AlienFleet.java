967e2f717e72a51f3978c645aba3059099d26584
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-03-12 16:30:53 -0700

    Add another row of aliens

--- src/edu/osucascades/cs361/chi/AlienFleet.java
+++ src/edu/osucascades/cs361/chi/AlienFleet.java
@@ -6,7 +6,7 @@ public class AlienFleet {
     private Sketch canvas;
     private int x;
     private int y;
-    private static final int ALIEN_FLEET_ROWS = 1;    //Real game uses 5
+    private static final int ALIEN_FLEET_ROWS = 2;    //Real game uses 5
     private static final int ALIEN_FLEET_COLUMNS = 6;  //Real game uses 11
     public Alien[][] aliens;
 
@@ -21,7 +21,7 @@ public class AlienFleet {
         aliens = new Alien[ALIEN_FLEET_ROWS][ALIEN_FLEET_COLUMNS];
         for (int r = 0; r < ALIEN_FLEET_ROWS; r++) {
             for (int c = 0; c < ALIEN_FLEET_COLUMNS; c++) {
-                Alien a = new Alien(c * 50, y, canvas);
+                Alien a = new Alien(c*50, y + (r*50), canvas);
                 aliens[r][c] = a;
                 collidables.add(a);
                 entities.add(a);
