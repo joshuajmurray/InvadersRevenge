3283d3c1d5ccd0106b14861ab5fa9b2ddb2d87ad
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-03-12 11:57:38 -0700

    Add Alien Fleet class base

---
+++ src/edu/osucascades/cs361/chi/AlienFleet.java
@@ -0,0 +1,16 @@
+package edu.osucascades.cs361.chi;
+import processing.core.PApplet;
+import java.util.ArrayList;
+
+public class AlienFleet {
+    private PApplet canvas;
+    private int x;
+    private int y;
+
+
+    AlienFleet(int x, int y, PApplet canvas) {
+
+    }
+
+
+}
