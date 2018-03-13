537ebea8ea07b5c0411fff8a3a595bb55dfc0389
joshua murray <joshuajmurray@gmail.com>
2018-03-12 22:31:31 -0700

    Implement sort for high score and username in scoremanager class Fixes #49

--- src/edu/osucascades/cs361/chi/ScoreManager.java
+++ src/edu/osucascades/cs361/chi/ScoreManager.java
@@ -2,29 +2,43 @@ package edu.osucascades.cs361.chi;
 
 import processing.core.PApplet;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import static processing.core.PApplet.println;
 
 public class ScoreManager {
 
     private PApplet score;
+    int hiScore = 0;
+    String playerName = "";
 
     ScoreManager(PApplet currentP) {
         score = currentP;
     }
 
     public int readScoresFromFile() {
-        int hiScore = 0;
         try {
-            String[] test =  score.loadStrings("highScore.txt");
-            int arraySize = test.length;
-            //TODO sort array for highest score first
-            System.out.println("there are " + arraySize + " lines");
-            for (int i = 0; i < arraySize; i++) {
-                String[] nameScoreSplit = test[i].split(" ");
-                hiScore = Integer.parseInt(nameScoreSplit[1]);
-            }
+            println(getHighScore(score.loadStrings("highScore.txt")));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return hiScore;
-    }}
+    }
+
+    public String getHighScore(String[] arrayDataIn) {
+        int arraySize = arrayDataIn.length;
+
+        for (int i = 0; i < arraySize; i++) {
+            int newScore = 0;
+            String[] nameScoreSplit = arrayDataIn[i].split(" ");
+            newScore = Integer.parseInt(nameScoreSplit[1]);
+            if(newScore > hiScore) {
+                hiScore = newScore;
+                playerName = nameScoreSplit[0];
+            }
+        }
+        return playerName + " " + hiScore;
+    }
+
+}
